07eecbe17a5c4f132c0e82e9440cc184140483a4
enax93 <emmacollado93@gmail.com>
2024-05-18 01:46:34 +0200

    Sprint 14 Practica Obligatoria Unidad 2

--- 04_Machine_Learning/Sprint_14/Unidad_02/Practica_Obligatoria/bootcampviztools.py
+++ 04_Machine_Learning/Sprint_14/Unidad_02/Practica_Obligatoria/bootcampviztools.py
@@ -274,3 +274,23 @@ def bubble_plot(df, col_x, col_y, col_size, scale = 1000):
     plt.show()
 
 
+def plot_predictions_vs_actual(y_real, y_pred):
+    """
+    Función para graficar los valores reales vs. los valores predichos en una regresión.
+
+    Args:
+    y_real (array-like): Valores reales de la variable objetivo.
+    y_pred (array-like): Valores predichos de la variable objetivo.
+    """
+    plt.figure(figsize=(8, 6))
+    plt.scatter(y_pred, y_real, alpha=0.5)
+    plt.xlabel("Valores Predichos")
+    plt.ylabel("Valores Reales")
+
+    # Línea y=x
+    max_value = max(max(y_real), max(y_pred))
+    min_value = min(min(y_real), min(y_pred))
+    plt.plot([min_value, max_value], [min_value, max_value], 'r')
+
+    plt.title("Comparación de Valores Reales vs. Predichos")
+    plt.show()
